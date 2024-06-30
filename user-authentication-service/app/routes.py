6cfa3cca194a880f9b3f2ea9fe4cfbed5570f796
Navdeep Tripathi <navi.14121997@gmail.com>
2024-06-30 19:03:38 +0530

    first commit

---
+++ user-authentication-service/app/routes.py
@@ -0,0 +1,29 @@
+from flask import request, jsonify
+from . import app, db, bcrypt, jwt
+from .models import User
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    new_user = User(username=data['username'], password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(message="User created successfully"), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+    if user and bcrypt.check_password_hash(user.password, data['password']):
+        access_token = create_access_token(identity={'username': user.username})
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify(message="Invalid credentials"), 401
+
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
